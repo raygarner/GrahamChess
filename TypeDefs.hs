29af95e1065ce8707e20757396137c7ee7e0ddee
Ray Garner <ray@perfectcast.co.uk>
2020-04-15 14:20:41 +0100

    added data type for piece type

--- TypeDefs.hs
+++ TypeDefs.hs
@@ -1,12 +1,17 @@
 module TypeDefs where
 
+data PieceType = Pawn | Knight | Bishop | Rook | Queen | King
+                 deriving (Eq, Ord)
+
 type Col = Bool
 
 type Pos = (Int, Int) -- row, column
 
-type Piece = (Char, Col, Pos) -- type, colour of piece on square, position
+type Piece = (PieceType, Col, Pos) -- type, colour of piece on square, position
 
 type AllPieces = [Piece]
 
 type Move = (Piece, Pos, Bool) -- piece, target square, validity
+
+
 
