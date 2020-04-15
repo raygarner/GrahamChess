22ae023c98259b53e1808ef17c369632b97cce3d
Ray Garner <ray@perfectcast.co.uk>
2020-04-15 13:59:12 +0100

    added function to see whether proposed move is in a straight line. compiles but needs testing

--- util.hs
+++ util.hs
@@ -3,24 +3,24 @@ import TypeDefs
 -- GETTERS
 
 -- returns the column of a position
-getcolumn :: Pos -> Int
-getcolumn (_,y) = y
+getColumn :: Pos -> Int
+getColumn (_,y) = y
 
 -- returns the row of a position
-getrow :: Pos -> Int
-getrow (x,_) = x
+getRow :: Pos -> Int
+getRow (x,_) = x
 
 -- returns the colour of a piece
-getcolour :: Piece -> Col
-getcolour (_,x,_) = x
+getColour :: Piece -> Col
+getColour (_,x,_) = x
 
 -- returns the position of a piece
-getpos :: Piece -> Pos
-getpos (_,_,x) = x
+getPos :: Piece -> Pos
+getPos (_,_,x) = x
 
 -- returns the starting position of a move
 getStart :: Move -> Pos
-getStart (x,_,_) = getpos x
+getStart (x,_,_) = getPos x
 
 -- returns the target position for a move
 getTarget :: Move -> Pos
@@ -28,11 +28,11 @@ getTarget (_,y,_) = y
 
 -- returns the piece that is on a square in a list (empty list if no piece there)
 getPiece :: Pos -> AllPieces -> [Piece]
-getPiece a b = [x | x <- b, getpos x == a]
+getPiece a b = [x | x <- b, getPos x == a]
 
 
 
--- UTILITIES
+-- UTILITIES AND RULES
 
 -- returns true if there is no piece on pos
 isEmpty :: Pos -> AllPieces -> Bool
@@ -40,7 +40,7 @@ isEmpty a b = (getPiece a b) == []
 
 -- returns true if the two pieces are enemies
 isEnemy :: Piece -> Piece -> Bool
-isEnemy a b = getcolour a /= getcolour b
+isEnemy a b = getColour a /= getColour b
 
 -- returns whether a square is not occupied by a friendly piece
 isValidTarget :: Move -> AllPieces -> Bool
@@ -48,4 +48,11 @@ isValidTarget a b = isEmpty (getTarget a) b || isEnemy s t
                     where
                         s = head (getPiece (getStart a) b)
                         t = head (getPiece (getTarget a) b)
+
+-- returns whether a move is in a straight line or not
+isStraightMove :: Move -> Bool
+isStraightMove a = (colDiff == rowDiff) || (colDiff == 0 || rowDiff == 0)
+                   where
+                       colDiff = abs (getColumn (getTarget a)) - (getColumn (getStart a))
+                       rowDiff = abs (getRow (getTarget a)) - (getRow (getStart a))
 
