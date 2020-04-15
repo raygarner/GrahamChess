ff996b38a3b6ab2dcbe51466fccf8a2afd8139ce
Ray Garner <ray@perfectcast.co.uk>
2020-04-15 19:44:40 +0100

    started debug file

--- debug.hs
+++ debug.hs
@@ -3,6 +3,7 @@ import TypeDefs
 -- Generate starting piece
 --genStartingBoard :: AllPieces
 
---addPawns :: Col -> AllPieces
---addPawns True = [ (Pawn, c, (x, b) | a <- [1, 6], b <- [0..7],
+addPawns :: Col -> AllPieces
+addPawns White = [ (Pawn, White, (7, y) | y <-
+
 
