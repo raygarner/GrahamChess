553f7d454b99c5062ebd9f7e072d4d5b1f322472
tomcotter7 <thomascotter00@gmail.com>
2020-04-15 22:32:26 +0100

    isValidMove Rook

--- util.hs
+++ util.hs
@@ -58,14 +58,23 @@ closerToZero a | a < 0 = a + 1
                | otherwise = a - 1
 
 -- decrease the value of a move by 1 closer to the original position
-decreaseMove :: Move -> Move
-decreaseMove (a,b) = (closerToZero a, closerToZero b)
+decreaseDiagonalMove :: Move -> Move
+decreaseDiagonalMove (a,b) = (closerToZero a, closerToZero b)
+
 
 --checks to see if a piece can move along a diagonal line without hitting any pieces.
 isDiagonalMovePathEmpty :: Pos -> Move -> AllPieces -> Bool
 isDiagonalMovePathEmpty a (0,0) c = True
 isDiagonalMovePathEmpty a b c = isEmpty (getTarget a b2) c && isDiagonalMovePathEmpty a b2 c
-                                where b2 = decreaseMove b
+                                where b2 = decreaseDiagonalMove b
+
+-- checks to see if a piece can move along a straight line without hitting any pieces.
+isStraightMovePathEmpty :: Pos -> Move -> AllPieces -> Bool
+isStraightMovePathEmpty a (0,0) c = True
+isStraightMovePathEmpty a (0,b) c = isEmpty (getTarget a (0,b2)) c && isStraightMovePathEmpty a (0,b2) c
+                                    where b2 = closerToZero b
+isStraightMovePathEmpty a (b,0) c = isEmpty (getTarget a (b2,0)) c && isStraightMovePathEmpty a (b2,0) c
+                                    where b2 = closerToZero b
 
 --returns whether a pawn move is a capture
 isPawnCapture :: Piece -> Move -> Bool
@@ -79,7 +88,7 @@ isBasicPawnMove (_,White,_) (a,b) = b == -1 && a == 0
 
 -- returns whether a move is in a straight line or not
 isStraightMove :: Move -> Bool
-isStraightMove (a,b) = (a == b) || (a == 0 || b == 0)
+isStraightMove (a,b) = (a == 0 || b == 0)
 
 -- return whether a move is a diagonal
 isDiagonal :: Move -> Bool
@@ -99,9 +108,15 @@ isKnightValidMove a b c = isValidTarget a b c && isLShaped b
 
 -- returns whether a bishop move is valid
 isBishopValidMove :: Piece -> Move -> AllPieces -> Bool
-isBishopValidMove a b c = isValidTarget a b c && isDiagonal b && isDiagonalMovePathEmpty (getPos a) b c
+isBishopValidMove a b c | isDiagonal b = isValidTarget a b c && isDiagonalMovePathEmpty (getPos a) b c
+                        | otherwise = False
+
+isRookValidMove :: Piece -> Move -> AllPieces -> Bool
+isRookValidMove a b c | isStraightMove b = isValidTarget a b c && isStraightMovePathEmpty (getPos a) b c
+                      | otherwise = False
 
 isValidMove :: Piece -> Move -> AllPieces -> Bool
 isValidMove (Pawn, col, pos) x y   = isPawnValidMove (Pawn, col, pos) x y
 isValidMove (Knight, col, pos) x y = isKnightValidMove (Knight, col, pos) x y
 isValidMove (Bishop, col, pos) x y = isBishopValidMove (Bishop, col, pos) x y
+isValidMove (Rook, col, pos) x y   = isRookValidMove (Rook, col, pos) x y
