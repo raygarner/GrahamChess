fa062fa159099ec7d56e88a3a84c854f359d6780
Ray Garner <ray@perfectcast.co.uk>
2020-04-22 10:22:08 +0100

    fixed compilation errors in main

--- Main.hs
+++ Main.hs
@@ -34,19 +34,19 @@ contains :: String -> String -> Bool
 contains a b = isInfixOf a b
 
 -- returns a piece from 4 strings
-createPiece :: String -> String -> String -> String -> Piece
+--createPiece :: String -> String -> String -> String -> Piece
 --createPiece a b c d = (pieceType a, colorType b, position c d)
-createPiece a b c d = (read a, read b, (read c, read d))
+--createPiece a b c d = (read a, read b, (read c, read d))
 
 
 -- builds a piece from a list of info
 buildPiece :: [String] -> Piece
 buildPiece (piece:colour:row:column:moves:_) = (read piece, read colour, (read row, read column), read moves)
-buildPiece _ = (Rook,Black,(0,0)) --never ran, compiler just wanted a default
+buildPiece _ = (Rook,Black,(0,0),0) --never ran, compiler just wanted a default
 
 -- builds a move from a list of info
 buildMove :: [String] -> Move
-buildMove (_:_:_:_:m:n:_) = (read m, read (head n))
+buildMove (_:_:_:_:m:n:_) = (read m, read n)
 buildMove _ = (0,0) --never ran, compiler just wanted a default
 
 -- if you can think of nicer ways to do this function then let me know.
