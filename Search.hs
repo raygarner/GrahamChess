b19bcd42ab680b93651bf0c0bafa2d6224f3b163
Ray Garner <ray@perfectcast.co.uk>
2020-04-25 14:10:53 +0100

    added missing parameter in Search.hs

--- Search.hs
+++ Search.hs
@@ -9,12 +9,12 @@ import Eval
 -- returns the strongest move which can be made in a position
 --findBestMove :: Int -> Colour -> AllPieces -> (Piece,Move)
 --findBestMove l c ps | l == 5 = findSingleBestMove c ps
---                    | otherwise findSingleBestMove (makeSingleBestMove (findBestMove (++l) (invertColour c) ps))
+--                    | otherwise findSingleBestMove c (makeSingleBestMove (findBestMove (++l) (invertColour c) ps))
 
 
 -- returns the best move which can be made without looking ahead
 --findSingleBestMove :: Colour -> AllPieces -> (Piece, Move)
-
+--findSingleBestMove
 
 
 --makeSingleBestMove :: (Piece, Move) -> AllPieces -> AllPieces
