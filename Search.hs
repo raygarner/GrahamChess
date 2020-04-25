aa5db245a8cb685cbd0a3da3755a6def53c1e85b
Ray Garner <ray@perfectcast.co.uk>
2020-04-25 14:07:42 +0100

    added some pseudocode for searching

--- Search.hs
+++ Search.hs
@@ -6,8 +6,22 @@ import Init
 import Util
 import Eval
 
---findBestMove :: Colour -> AllPieces -> Move
---findBestMove c ps =
+-- returns the strongest move which can be made in a position
+--findBestMove :: Int -> Colour -> AllPieces -> (Piece,Move)
+--findBestMove l c ps | l == 5 = findSingleBestMove c ps
+--                    | otherwise findSingleBestMove (makeSingleBestMove (findBestMove (++l) (invertColour c) ps))
+
+
+-- returns the best move which can be made without looking ahead
+--findSingleBestMove :: Colour -> AllPieces -> (Piece, Move)
+
+
+
+--makeSingleBestMove :: (Piece, Move) -> AllPieces -> AllPieces
+--makeSingleBestMove (a,b) ps = movePiece m n ps
 
 -- makes a move and then evaluates the new AllPieces
---evalMove :: Move -> AllPieces -> Float
+evalMove :: Piece -> Move -> AllPieces -> Float
+evalMove a m ps = totalVal (getColour a) (movePiece a m ps)
+
+--
