51075b2fb51285c1157bca927473402037e16f43
Ray Garner <ray@perfectcast.co.uk>
2020-04-25 14:35:07 +0100

    added function to find best move from list now

--- Search.hs
+++ Search.hs
@@ -7,14 +7,22 @@ import Util
 import Eval
 
 -- returns the strongest move which can be made in a position
---findBestMove :: Int -> Colour -> AllPieces -> (Piece,Move)
+--findBestMove :: Int -> Colour -> AllPieces -> (Piece,Move,Float)
 --findBestMove l c ps | l == 5 = findSingleBestMove c ps
 --                    | otherwise findSingleBestMove c (makeSingleBestMove (findBestMove (++l) (invertColour c) ps))
 
 
 -- returns the best move which can be made without looking ahead
---findSingleBestMove :: Colour -> AllPieces -> (Piece, Move)
---findSingleBestMove c ps =
+findSingleBestMove :: Colour -> AllPieces -> (Piece, Move, Float)
+findSingleBestMove c ps = findStrongestMoveFromAll (makeEvalList c ps)
+
+-- returns the stronget move from a list of moves with evaluations
+findStrongestMoveFromAll :: [(Piece,Move,Float)] -> (Piece,Move,Float)
+findStrongestMoveFromAll [(p,m,f)] = (p,m,f)
+findStrongestMoveFromAll ((p,m,f):xs) = if f > getMoveEval (findStrongestMoveFromAll xs) then (p,m,f) else findStrongestMoveFromAll xs
+
+getMoveEval :: (Piece, Move, Float) -> Float
+getMoveEval (_,_,f) = f
 
 makeEvalList :: Colour -> AllPieces -> [(Piece, Move, Float)]
 makeEvalList c ps = [ (x,y,evalMove x y ps) | x <- ps, getColour x == c, y <- legalMoves x ps ]
