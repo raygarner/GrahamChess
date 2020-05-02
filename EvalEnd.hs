4b4aa653c6b38c7ee9240f04a8c6f594a5a37804
tomcotter7 <thomascotter00@gmail.com>
2020-05-02 22:09:42 +0100

    new eval ideas - split up into opening -> end

---
+++ EvalEnd.hs
@@ -0,0 +1,8 @@
+module EvalEnd where
+
+import           TypeDefs
+import           Util
+import           Debug
+
+totalEndVal :: Colour -> AllPieces -> Float
+totalEndVal c ps = 100.0
---
+++ EvalMiddle.hs
@@ -0,0 +1,8 @@
+module EvalMiddle where
+
+import           TypeDefs
+import           Util
+import           Debug
+
+totalMiddleVal :: Colour -> AllPieces -> Float
+totalMiddleVal c ps = 100.0
