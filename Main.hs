534946d78dd5790b2790a2466825fb196d8f8e09
tomcotter7 <thomascotter00@gmail.com>
2020-05-02 22:53:10 +0100

    updated main so its easier to play against

--- Main.hs
+++ Main.hs
@@ -27,19 +27,28 @@ gameLoop ps = do printBoard 0 ps
                  print piece
                  move <- return (buildMove (r,c))
                  print move
-                 ps <- return (movePiece piece move ps)
-                 print ps
-                 printBoard 0 ps
-                 putStr "Graham is thinking of a move...\n"
-                 response <- return (findRealBestMove Black ps)
-                 print response
-                 putStr "Graham has made his move...\n"
-                 move <- return (extractMove response)
-                 print move
-                 piece <- return (extractPiece response)
-                 print piece
-                 gameLoop (movePiece piece move ps)
+                 if (isMoveOk piece move ps) then
+                   do
+                     ps <- return (executeMove piece move ps)
+                     print ps
+                     printBoard 0 ps
+                     putStr "Graham is thinking of a move...\n"
+                     response <- return (findRealBestMove Black ps)
+                     print response
+                     putStr "Graham has made his move...\n"
+                     move <- return (extractMove response)
+                     print move
+                     piece <- return (extractPiece response)
+                     print piece
+                     gameLoop (executeMove piece move ps)
+                 else
+                   do
+                     putStr ("Move is invalid - either your king is in check or your piece cannot move there.\n")
+                     gameLoop ps
 
 
 buildMove :: (String, String) -> (Int,Int)
 buildMove (r,c) = (read r, read c)
+
+isMoveOk :: Piece -> Move -> AllPieces -> Bool
+isMoveOk p m ps = isValidMove p m ps && not (willKingBeInCheck p m ps)
