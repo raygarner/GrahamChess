2de0000cb72d13dcc56504e64bd89d16cc78c834
Ray Garner <ray@perfectcast.co.uk>
2020-06-05 18:54:55 +0100

    tweaked opening

--- EvalOpening.hs
+++ EvalOpening.hs
@@ -8,13 +8,109 @@ import           Control.Parallel
 
 -- some crude evaluations
 
+evalPawn :: Colour -> Pos -> Int
+evalPawn c (m,n) = relboard !! i
+               where
+                   i = (m * 8) + n
+                   board = [ 0,  0,  0,  0,  0,  0,  0,  0,
+                            50, 50, 50, 50, 50, 50, 50, 50,
+                            10, 10, 20, 30, 30, 20, 10, 10,
+                             5,  5, 10, 25, 25, 10,  5,  5,
+                             0,  0,  0, 20, 20,  0,  0,  0,
+                             5, -5,-10,  0,  0,-10, -5,  5,
+                             5, 10, 10,-20,-20, 10, 10,  5,
+                             0,  0,  0,  0,  0,  0,  0,  0]
+                   relboard = if c==White then board else reverse board
+
+
+evalKnight :: Colour -> Pos -> Int
+evalKnight c (m,n) = relboard !! i
+                     where
+                         i = (m * 8) + n
+                         board = [-50,-40,-30,-30,-30,-30,-40,-50,
+                                -40,-20,  0,  0,  0,  0,-20,-40,
+                                -30,  0, 10, 15, 15, 10,  0,-30,
+                                -30,  5, 15, 20, 20, 15,  5,-30,
+                                -30,  0, 15, 20, 20, 15,  0,-30,
+                                -30,  5, 10, 15, 15, 10,  5,-30,
+                                -40,-20,  0,  5,  5,  0,-20,-40,
+                                -50,-40,-30,-30,-30,-30,-40,-50]
+                         relboard = if c==White then board else reverse board
+
+evalBishop :: Colour -> Pos -> Int
+evalBishop c (m,n) = relboard !! i
+                     where
+                         i = (m * 8) + n
+                         board = [-20,-10,-10,-10,-10,-10,-10,-20,
+                                -10,  0,  0,  0,  0,  0,  0,-10,
+                                -10,  0,  5, 10, 10,  5,  0,-10,
+                                -10,  5,  5, 10, 10,  5,  5,-10,
+                                -10,  0, 10, 10, 10, 10,  0,-10,
+                                -10, 10, 10, 10, 10, 10, 10,-10,
+                                -10,  5,  0,  0,  0,  0,  5,-10,
+                                -20,-10,-10,-10,-10,-10,-10,-20]
+                         relboard = if c==White then board else reverse board
+
+evalRook :: Colour -> Pos -> Int
+evalRook c (m,n) = relboard !! i
+                     where
+                         i = (m * 8) + n
+                         board = [ 0,  0,  0,  0,  0,  0,  0,  0,
+                                  5, 10, 10, 10, 10, 10, 10,  5,
+                                 -5,  0,  0,  0,  0,  0,  0, -5,
+                                 -5,  0,  0,  0,  0,  0,  0, -5,
+                                 -5,  0,  0,  0,  0,  0,  0, -5,
+                                 -5,  0,  0,  0,  0,  0,  0, -5,
+                                 -5,  0,  0,  0,  0,  0,  0, -5,
+                                  0,  0,  0,  5,  5,  0,  0,  0]
+                         relboard = if c==White then board else reverse board
+
+evalQueen :: Colour -> Pos -> Int
+evalQueen c (m,n) = (relboard !! i)
+                     where
+                         i = (m * 8) + n
+                         board = [-20,-10,-10, -5, -5,-10,-10,-20,
+                                -10,  0,  0,  0,  0,  0,  0,-10,
+                                -10,  0,  5,  5,  5,  5,  0,-10,
+                                 -5,  0,  5,  5,  5,  5,  0, -5,
+                                  0,  0,  5,  5,  5,  5,  0, -5,
+                                -10,  5,  5,  5,  5,  5,  0,-10,
+                                -10,  0,  5,  0,  0,  0,  0,-10,
+                                -20,-10,-10, -5, -5,-10,-10,-20]
+                         relboard = if c==White then board else reverse board
+
+evalKing :: Colour -> Pos -> Int
+evalKing c (m,n) = relboard !! i
+                     where
+                         i = (m * 8) + n
+                         board = [-30,-40,-40,-50,-50,-40,-40,-30,
+                                -30,-40,-40,-50,-50,-40,-40,-30,
+                                -30,-40,-40,-50,-50,-40,-40,-30,
+                                -30,-40,-40,-50,-50,-40,-40,-30,
+                                -20,-30,-30,-40,-40,-30,-30,-20,
+                                -10,-20,-20,-20,-20,-20,-20,-10,
+                                 20, 20,  0,  0,  0,  0, 20, 20,
+                                 20, 30, 10,  0,  0, 10, 30, 20]
+                         relboard = if c==White then board else reverse board
+
+
+
+
 evalPiece :: Piece -> AllPieces -> Int
 --evalPiece (King,_,_,_) ps = 0.0
 --evalPiece (Pawn,_,_,_) ps = 0.0
 --evalPiece (Rook,_,_,_) ps = 0.0
 --evalPiece (Queen,_,_,_) ps = 0.0
 --evalPiece a ps = fromIntegral (length (legalMoves a [a]) + length (legalMoves a ps)) -- * pieceMobMult a
-evalPiece a ps = length (legalMoves a [a]) + length (legalMoves a ps) `div` 2-- * pieceMobMult a
+--evalPiece a ps = length (legalMoves a [a]) + length (legalMoves a ps) `div` 2-- * pieceMobMult a
+--evalPiece a ps = 3 * length (legalMoves a [a])
+--evalPiece a ps = length (legalMoves a ps)
+evalPiece (Pawn,c,(m,n),mc) ps = evalPawn c (m,n)
+evalPiece (Knight,c,(m,n),mc) ps = evalKnight c (m,n)
+evalPiece (Bishop,c,(m,n),mc) ps = evalBishop c (m,n)
+evalPiece (Rook,c,(m,n),mc) ps = evalRook c (m,n)
+evalPiece (Queen,c,(m,n),mc) ps = evalQueen c (m,n)
+evalPiece (King,c,(m,n),mc) ps = evalKing c (m,n)
 
 --pieceMobMult :: Piece -> Float
 --pieceMobMult (Knight,_,_,_) = 1
@@ -23,7 +119,7 @@ evalPiece a ps = length (legalMoves a [a]) + length (legalMoves a ps) `div` 2-- 
 --pieceMobMult (Bishop,_,_,_) = 1.0
 
 totalMaterial :: Colour -> AllPieces -> Int
-totalMaterial c ps = sum [pieceVal (y,White,(0,0),0) * countPieceType c y ps  | y <- pieceTypes]
+totalMaterial c ps = 2 * sum [pieceVal (y,White,(0,0),0) * countPieceType c y ps  | y <- pieceTypes]
 
 countPieceType :: Colour -> PieceType -> AllPieces -> Int
 countPieceType c t ps = length [ x | x <- ps, getColour x == c, getPieceType x == t, getPos x /= (-1,-1) ]
@@ -32,33 +128,35 @@ pieceTypes :: [PieceType]
 pieceTypes = [Pawn, Knight, Bishop, Rook, Queen, King]
 
 totalMobility :: Colour -> AllPieces -> Int
-totalMobility c ps = sum [ evalPiece x ps | x <- ps, (getPieceType x == Knight || getPieceType x == Bishop), getColour x == c, getPos x /= (-1,-1) ]
+totalMobility c ps = sum [ evalPiece x ps | x <- ps, getColour x == c, getPos x /= (-1,-1) ]
 
 totalOpeningVal :: AllPieces -> Int
 totalOpeningVal ps = totalOpeningValColour White ps - totalOpeningValColour Black ps
 
 totalOpeningValColour :: Colour -> AllPieces -> Int
 --totalOpeningValColour c ps = totalMaterial c ps + totalMobility c ps + centralPawns c ps + blockedPawns c ps + kingSafety c ps + queenSafety c ps
-totalOpeningValColour c ps = totalMaterial c ps + totalMobility c ps + kingSafety c ps
+--totalOpeningValColour c ps = totalMaterial c ps + totalMobility c ps + kingSafety c ps + queenSafety c ps + centralPawns c ps + blockedPawns c ps
+--totalOpeningValColour c ps = movePieceBonus c ps + totalMaterial c ps + kingSafety c ps + queenSafety c ps + centralPawns c ps + blockedPawns c ps
+totalOpeningValColour c ps = totalMaterial c ps + totalMobility c ps + queenSafety c ps
 
-centralPawns :: Colour -> AllPieces -> Float
-centralPawns c ps = 1.0 * (fromIntegral (length [x | x <- ps, getColour x == c, getPieceType x == Pawn, any (==getPos x) squares]))
+centralPawns :: Colour -> AllPieces -> Int
+centralPawns c ps = 1 * fromIntegral (length [x | x <- ps, getColour x == c, getPieceType x == Pawn, any (==getPos x) squares])
                     where
                         squares = [(3,3),(3,4),(4,3),(4,4)]
 
-blockedPawns :: Colour -> AllPieces -> Float
-blockedPawns c ps = fromIntegral (length [x | x <- ps, getColour x == c, (getColumn (getPos x) == 3 || getColumn (getPos x) == 4), length (legalMoves x ps) == 0]) * (-60)
+blockedPawns :: Colour -> AllPieces -> Int
+blockedPawns c ps = length [x | x <- ps, getColour x == c, (getColumn (getPos x) == 3 || getColumn (getPos x) == 4), length (legalMoves x ps) == 0] * (-60)
 
-queenSafety :: Colour -> AllPieces -> Float
-queenSafety c ps | q == (-1,-1) = 0.0
+queenSafety :: Colour -> AllPieces -> Int
+queenSafety c ps | q == (-1,-1) = 0
                  | otherwise = movedLessThan c queen ps
                    where
                        queen = head (findPiece q ps)
                        q = findQueen c ps
 
-movedLessThan :: Colour -> Piece -> AllPieces -> Float
-movedLessThan c p ps | mc == 0 = 0.0
-                     | otherwise = if null [x | x <- ps, getColour x == c, getMovecount x == 0, (getPieceType x == Knight || getPieceType x == Bishop)] then 0.0 else (-50)
+movedLessThan :: Colour -> Piece -> AllPieces -> Int
+movedLessThan c p ps | mc == 0 = 0
+                     | otherwise = if null [x | x <- ps, getColour x == c, getMovecount x == 0, (getPieceType x == Knight || getPieceType x == Bishop)] then 0 else (-50)
                        where
                            mc = getMovecount p
 
@@ -136,5 +234,5 @@ pieceVal (King,_,_,_)   = 0
 
 
 -- add bonus for moving multiple pieces.
-movePieceBonus :: Colour -> AllPieces -> Float
-movePieceBonus c ps = fromIntegral (length [x | x <- ps, getPos x /= (-1,-1), getColour x == c, getMovecount x == 0, getPieceType x == Knight || getPieceType x == Bishop]) * (-80)
+movePieceBonus :: Colour -> AllPieces -> Int
+movePieceBonus c ps = length [x | x <- ps, getPos x /= (-1,-1), getColour x == c, getMovecount x == 0, getPieceType x == Knight || getPieceType x == Bishop] * (-80)
